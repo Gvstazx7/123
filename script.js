cfc736bf46e1c3442d4cf42968c0faac52413ac0
Gvstazx7 <gvstavic22@gmail.com>
2025-05-06 01:06:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click', function() {
+  alert('Você aceitou! Vamos ser felizes juntos!');
+});
+
+document.getElementById('no').addEventListener('click', function() {
+  alert('Você não quis, mas vou te conquistar!');
+});
